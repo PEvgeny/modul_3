96c8f2ab4996bc6a7e1040c8714427b13317e1a3
Evgeniy <epolupanov@diasoft.ru>
2020-12-18 11:01:41 +0300

    Реализован микросервис библиотека

--- src/main/java/com/library/controller/LibraryController.java
+++ src/main/java/com/library/controller/LibraryController.java
@@ -9,41 +9,41 @@ import java.util.*;
 @RequestMapping("books")
 public class LibraryController {
 
-    private List<Map<String, Object>> library = new ArrayList<Map<String, Object>>(){{
-        add(new HashMap<String,Object>(){{ put("id", 1); put("name", "Война и мир"); }});
-        add(new HashMap<String,Object>(){{ put("id", 2); put("name", "Преступление и наказание"); }});
-        add(new HashMap<String,Object>(){{ put("id", 3); put("name", "Идиот"); }});
-        add(new HashMap<String,Object>(){{ put("id", 4); put("name", "Мастер и Маргарита"); }});
-        add(new HashMap<String,Object>(){{ put("id", 5); put("name", "Отцы и дети"); }});
+    private List<Map<String, String>> library = new ArrayList<Map<String, String>>(){{
+        add(new HashMap<String,String>(){{ put("id", "1"); put("name", "Война и мир"); }});
+        add(new HashMap<String,String>(){{ put("id", "2"); put("name", "Преступление и наказание"); }});
+        add(new HashMap<String,String>(){{ put("id", "3"); put("name", "Идиот"); }});
+        add(new HashMap<String,String>(){{ put("id", "4"); put("name", "Мастер и Маргарита"); }});
+        add(new HashMap<String,String>(){{ put("id", "5"); put("name", "Отцы и дети"); }});
     }};
 
     @GetMapping
-    public List<Map<String, Object>> library(){
+    public List<Map<String, String>> library(){
         return library;
     }
 
     @GetMapping("{id}")
-    public Map<String, Object> getBook(@PathVariable Integer id){
+    public Map<String, String> getBook(@PathVariable String id){
         return getBookById(id);
     }
 
-    private Map<String, Object> getBookById(Integer id){
+    private Map<String, String> getBookById(String id){
         return library.stream().filter(library -> library.get("id").equals(id)).findFirst()
             .orElseThrow(NotFoundException :: new);
     }
 
     @PostMapping
-    public Map<String, Object> addBook(@RequestBody Map<String, Object> book){
-        int curId = 1;
+    public Map<String, String> addBook(@RequestBody Map<String, String> book){
+        String curId = "1";
         if(library != null && !library.isEmpty()){
-            int maxId = (Integer) library.get(0).get("id");
-            for(Map<String, Object> bk : library){
-                int id = (Integer) bk.get("id");
-                if(maxId <= id){
+            String maxId = library.get(0).get("id");
+            for(Map<String, String> bk : library){
+                String id = bk.get("id");
+                if(maxId.compareTo(id) <= 0){
                     maxId = id;
                 }
             }
-            curId = maxId+1;
+            curId = String.valueOf(Integer.parseInt(maxId)+1);
         }
 
         book.put("id", curId);
@@ -52,16 +52,16 @@ public class LibraryController {
     }
 
     @PutMapping("{id}")
-    public Map<String, Object> updateBook(@RequestBody Map<String, Object> book, @PathVariable Integer id){
-        Map<String, Object> bookFromDb = getBookById(id);
+    public Map<String, String> updateBook(@RequestBody Map<String, String> book, @PathVariable String id){
+        Map<String, String> bookFromDb = getBookById(id);
         bookFromDb.putAll(book);
         bookFromDb.put("id", id);
         return bookFromDb;
     }
 
     @DeleteMapping("{id}")
-    public void deleteBook(@PathVariable Integer id){
-        Map<String, Object> book = getBookById(id);
+    public void deleteBook(@PathVariable String id){
+        Map<String, String> book = getBookById(id);
         library.remove(book);
     }
 }
